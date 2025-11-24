b20ed368bb477317a6eb93d103e44711d4205fe7
Tracy Atteberry <tracy@magicbydesign.com>
2025-11-23 18:47:40 -0600

    feat: add quality scores to formatted output
    Include validation metrics as comments when --show-scores flag is used.
    Support all output formats with appropriate comment syntax:
    - Ruby, Python: # comments
    - JavaScript, Java: // comments
    - Text: plain text
    - JSON: nested quality metadata object

--- lib/hyrum/formats/formatter.rb
+++ lib/hyrum/formats/formatter.rb
@@ -13,10 +13,14 @@ module Hyrum
         @options = options
       end
 
-      def format(messages)
+      def format(messages, validation_result = nil)
         template_file = File.join(__dir__, 'templates', "#{options[:format]}.erb")
         template = ERB.new(File.read(template_file), trim_mode: '-')
-        template.result_with_hash(messages: messages)
+        template.result_with_hash(
+          messages: messages,
+          validation_result: validation_result,
+          show_scores: options[:show_scores]
+        )
       end
     end
   end
